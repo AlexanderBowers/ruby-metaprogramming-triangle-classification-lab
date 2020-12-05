5d51a0b4e4611b425225d405b93d41aefe2c39df
AlexanderBowers <7319abowers@gmail.com>
2020-12-04 21:05:30 -0800

    finished lab

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,36 @@
 class Triangle
-  # write code here
+  attr_accessor :side1, :side2, :side3
+  def initialize(side1, side2, side3)
+    @side1 = side1
+    @side2 = side2
+    @side3 = side3    
+  end
+
+  def kind
+    type = ""
+    if @side1 == 0
+    #if @side1 + @side2 < @side3 || @side1 <= 0 || @side2 <= 0 || @side3 <= 0
+       begin
+         raise TriangleError
+       rescue TriangleError => error
+        puts error.message
+      end
+    else
+      if @side1 == @side2 && @side1 == @side3
+        type = :equilateral
+     elsif (@side1 == @side2 && @side1 != @side3) || (@side1 != @side2 && @side2 == @side3) || (@side1 != @side2 && @side1 == @side3)
+        type = :isosceles
+     elsif @side1 != @side2 && @side1 != @side3 && @side2 != @side3
+        type = :scalene
+      end
+    end
+  end
+
+   class TriangleError < StandardError
+     def message
+        "sum of sides1 and sides2 must be greater than side3. \n
+         Each side must be greater than zero."
+      end
+    end
+
 end
